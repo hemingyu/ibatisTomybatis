66ad73d86c84c7a65433307a837944688d272b32
shi.changcheng <shi983561865@163.com>
2017-04-21 16:30:11 +0800

    增加注释

--- ibatisToMyibatis.py
+++ ibatisToMyibatis.py
@@ -68,54 +68,59 @@ def replacefile(refile,fileName):
 		p_14_2='\$(.*?)\$'
 		data_14_2=re.findall(p_14_2,line)
 		#print data_14_1,data_14_2
-		if data_1:
-			for i in range(len(data_1)):
-				line.replace(data_1[i],'<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.i//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">')
-		if data_2:
-			for i in range(len(data_2)):
-				line=line.replace(data_2[i],'mapper')
-		if data_3:
-			for i in range(len(data_3)):
-				line=line.replace(data_3[i],'type')
-		if data_4:
-			for i in range(len(data_4)):
-				line=line.replace(data_4[i],'Type')
-		if data_5_1:
-			for i in range(len(data_5_1)):
-				line=line.replace(data_5_1[i],'<if test="'+data_5_2[i][1]+' != null and '+data_5_2[i][1]+' != \'\' ">\n	'+data_5_2[i][0]+'')
-		if data_6_1:
-			for i in range(len(data_6_1)):
-				line=line.replace(data_6_1[i],'<if test="'+data_6_2[i][0]+' != null and '+data_6_2[i][0]+' != \'\' ">\n	'+data_6_2[i][1]+'')
-		if data_7_1:
-			for i in range(len(data_7_1)):
-				line=line.replace(data_7_1[i],'<if test="'+data_7_2[i][0]+' != null and '+data_7_2[i][0]+' != \'\' "')
-		if data_8:
-			for i in range(len(data_8)):
-				line=line.replace(data_8[i],'and')
-		if data_9:
-			for i in range(len(data_9)):
-				line=line.replace(data_9[i],'</if>')
-		if data_10:
-			for i in range(len(data_10)):
-				line=line.replace(data_1i[i],'where 1=1')
-		if data_11:
-			for i in range(len(data_11)):
-				line=line.replace(data_11[i],'')
-		if data_12:
-			for i in range(len(data_12)):
-				line=line.replace(data_12[i],'')
-		if data_13_1:
-			for i in range(len(data_13_1)):
-				line=line.replace(data_13_1[i],'#{'+data_13_2[i]+'}')
-		if data_14_1:
-			for i in range(len(data_14_1)):
-				line=line.replace(data_14_1[i],'${'+data_14_2[i]+'}')
-		f.write(line)
+		try:
+			if data_1:
+				for i in range(len(data_1)):
+					line.replace(data_1[i],'<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.i//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">')
+			if data_2:
+				for i in range(len(data_2)):
+					line=line.replace(data_2[i],'mapper')
+			if data_3:
+				for i in range(len(data_3)):
+					line=line.replace(data_3[i],'type')
+			if data_4:
+				for i in range(len(data_4)):
+					line=line.replace(data_4[i],'Type')
+			if data_5_1:
+				for i in range(len(data_5_1)):
+					line=line.replace(data_5_1[i],'<if test="'+data_5_2[i][1]+' != null and '+data_5_2[i][1]+' != \'\' ">\n	'+data_5_2[i][0]+'')
+			if data_6_1:
+				for i in range(len(data_6_1)):
+					line=line.replace(data_6_1[i],'<if test="'+data_6_2[i][0]+' != null and '+data_6_2[i][0]+' != \'\' ">\n	'+data_6_2[i][1]+'')
+			if data_7_1:
+				for i in range(len(data_7_1)):
+					line=line.replace(data_7_1[i],'<if test="'+data_7_2[i][0]+' != null and '+data_7_2[i][0]+' != \'\' "')
+			if data_8:
+				for i in range(len(data_8)):
+					line=line.replace(data_8[i],'and')
+			if data_9:
+				for i in range(len(data_9)):
+					line=line.replace(data_9[i],'</if>')
+			if data_10:
+				for i in range(len(data_10)):
+					line=line.replace(data_1i[i],'where 1=1')
+			if data_11:
+				for i in range(len(data_11)):
+					line=line.replace(data_11[i],'')
+			if data_12:
+				for i in range(len(data_12)):
+					line=line.replace(data_12[i],'')
+			if data_13_1:
+				for i in range(len(data_13_1)):
+					line=line.replace(data_13_1[i],'#{'+data_13_2[i]+'}')
+			if data_14_1:
+				for i in range(len(data_14_1)):
+					line=line.replace(data_14_1[i],'${'+data_14_2[i]+'}')
+			f.write(line)
+		except Exception as err:  
+			print(err)
+			print(fileName+'文件修改失败')
 	f.close()
+	print(fileName+'文件修改成功')
 
 def preimport(path):
 				'''
-				打印一个目录下的所有文件夹和文件 
+				匹配文件是否是xml后缀 
 				'''
 				Xfile=[]
 				for root, dirs, files in os.walk(path):
