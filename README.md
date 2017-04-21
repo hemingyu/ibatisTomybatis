6a6ba705f80728687eca0e1eb29f3817a697fc72
shi.changcheng <shi983561865@163.com>
2017-04-21 16:11:16 +0800

    pythothon文件和说明

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # ibatisTomybatis
+一定要按照这种顺序：
+java文件修改：
+
+将导入的包修改：
+org.springframework.orm.ibatis.support.SqlMapClientDaoSupport  --org.mybatis.spring.support.SqlSessionDaoSupport
+
+SqlMapClientDaoSupport   ---   SqlSessionDaoSupport
+
+getSqlMapClientTemplate  ---   getSqlSession
+
+queryForObject    ------   selectOne
+
+queryForList      ---------  selectList
+
+注释了批量操作，因为在myibatis在java里没有批量操作
+/fotamgr-5/src/main/java/com/ishengji/download/db/BatchExecute.java(spring里涉及了也要注释)
+
+xml文件修改：
+<!DOCTYPE sqlMap PUBLIC "-//ibatis.apache.org//DTD SQL Map 2.0//EN" "http://ibatis.apache.org/dtd/sql-map-2.dtd">  ----
+<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+
+sqlMap  ----- mapper
+
+class --- type
+
+Class --- Type
+
+（
+isNull判断property字段是否是null，用isEmpty更方便，包含了null和空字符串
+isNotNull对应 test="$2 != null"
+isNotEmpty对应 test="$2 != null and $2 != '' " "
+传入的map或者类的属性name等于"1"吗，是就附加and和vvvv = '哈哈'
+<isEqual property="name" compareValue="1" prepend="and">
+vvvv = '哈哈'
+< /isEqual>
+对应
+）
+
+<isNotEmpty.*?prepend=\"(.*?)\".?property=\"(.*?)\"?> -----
+<if test="$2 != null and $2 != '' ">\n	$1
+
+<isNotEmpty.*?property=\"(.*?)\".?prepend=\"(.*?)\"?> -----
+<if test="$1 != null and $1 != '' ">\n	$2
+
+<isNotEmpty.*?property=\"(.*?)\"  ----   <if test="$1 != null and $1 != '' "
+
+<isNotEmpty  prepend="and" >---  and
+注意：如果使用了<if ..> 关闭标签也必须由</isNotEmpty> 更改为</if>,<isNotNull>也这样修改
+
+</isNotEmpty.*?> ---  </if>
+
+<dynamic prepend="WHERE"?> ---  where 1=1修改要小心，要结合上下文修改，与ibatis属性有关系（http://blog.csdn.net/huyanzhiwei/article/details/52186265）
+
+<dynamic>  </dynamic?>  ---- 改为空
+
+
+<iterate  conjunction ="," >  ----  <foreach  item="item" collection="list" separator ="," >
+
+
+
+#value#需要改成:#{value}
+#(.*?)#  ------  #{$1}  一定要小心，易出错(#[^{](.*?)#  ------  #{$1})
+选择这种实验
+#{username} and BINARY u.password=#{password}
+= #userid#
+$value$需要改成:${value}
+\$(.*?)\$  ------  \${$1}
+
+spring.xml修改：
+sqlMapClient  ----  sqlSessionFactory
+
+出现的错误：
+Caused by: java.lang.IllegalArgumentException: Result Maps collection already contains value for newDown.DeltaByidResult
+表示有个mapper里有重复的id为DeltaByidResult的resultMap
+
+返回500,打开F12
+
+将错误复制出来查看
+java.lang.NoClassDefFoundError: Could not initialize class com.ishengji.ota.Tools.PropsUtil
+	com.ishengji.ota.Action.OperationProjectConfigAction.<init>(OperationProjectConfigAction.java:77)
+将PropsUtil.class.getResourceAsStream("/logPath.properties")修改为
+PropsUtil.class.getResourceAsStream("/conf/logPath.properties")
+（http://personbeta.iteye.com/blog/986241前面有 “   / ” 
+“ / ”代表了工程的根目录，例如工程名叫做myproject，“ / ”代表了myproject 
+me.class.getResourceAsStream("/com/x/file/myfile.xml"); ）
---
+++ ibatisToMyibatis.py
@@ -0,0 +1,131 @@
+﻿#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os , re, sys
+
+#replacefile转换ibatis为myibatis,针对mapper文件
+def replacefile(refile,fileName):
+	print refile
+	if not refile.strip():
+		print 'refile is null'
+	print '正在操作'+fileName+'文件'
+
+	f=open(refile,'r+')
+	all_lines=f.readlines()
+	f.seek(0)
+	f.truncate()
+	for line in all_lines:
+		#匹配ibatis头
+		p_1='<!DOCTYPE sqlMap.*?>'
+		data_1=re.findall(p_1,line)
+		#匹配sqlMap
+		p_2='sqlMap'
+		data_2=re.findall(p_2,line)
+		#匹配class
+		p_3='class'
+		data_3=re.findall(p_3,line)
+		#匹配XXXClass
+		p_4='Class'
+		data_4=re.findall(p_4,line)
+		#匹配isNotEmpty,isNotNull,isEqual
+		p_5_1='<isNotEmpty.*?prepend=\".*?\".?property=\".*?\"?>'
+		data_5_1=re.findall(p_5_1,line)
+		p_5_2='<isNotEmpty.*?prepend=\"(.*?)\".?property=\"(.*?)\"?>'
+		data_5_2=re.findall(p_5_2,line)
+		
+		p_6_1='<isNotEmpty.*?property=\".*?\".?prepend=\".*?\"?>'
+		data_6_1=re.findall(p_6_1,line)
+		p_6_2='<isNotEmpty.*?property=\"(.*?)\".?prepend=\"(.*?)\"?>'
+		data_6_2=re.findall(p_6_2,line)
+		
+		p_7_1='<isNotEmpty.*?property=\".*?\"'
+		data_7_1=re.findall(p_7_1,line)	
+		p_7_2='<isNotEmpty.*?property=\"(.*?)\"'
+		data_7_2=re.findall(p_7_2,line)
+		
+		p_8='<isNotEmpty  prepend="and".*?>'
+		data_8=re.findall(p_8,line)
+		#匹配</isNotEmpty结尾
+		p_9='</isNotEmpty.*?>'
+		data_9=re.findall(p_9,line)
+		#匹配<dynamic prepend="WHERE"?>
+		p_10='<dynamic prepend="WHERE"?>'
+		data_10=re.findall(p_10,line)
+		#匹配<dynamic>
+		p_11='<dynamic>'
+		data_11=re.findall(p_11,line)
+		#匹配</dynamic?>结尾
+		p_12='</dynamic?>'
+		data_12=re.findall(p_12,line)
+		#匹配#value#需要改成:#{value}
+		p_13_1='#.*?#'
+		data_13_1=re.findall(p_13_1,line)
+		p_13_2='#(.*?)#'
+		data_13_2=re.findall(p_13_2,line)
+		#print data_13_1,data_13_2
+		#匹配$value$需要改成:${value}
+		p_14_1='\$.*?\$'
+		data_14_1=re.findall(p_14_1,line)
+		p_14_2='\$(.*?)\$'
+		data_14_2=re.findall(p_14_2,line)
+		print data_14_1,data_14_2
+		if data_1:
+			for i in range(len(data_1)):
+				line.replace(data_1[i],'<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.i//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">')
+		if data_2:
+			for i in range(len(data_2)):
+				line=line.replace(data_2[i],'mapper')
+		if data_3:
+			for i in range(len(data_3)):
+				line=line.replace(data_3[i],'type')
+		if data_4:
+			for i in range(len(data_4)):
+				line=line.replace(data_4[i],'Type')
+		if data_5_1:
+			for i in range(len(data_5_1)):
+				line=line.replace(data_5_1[i],'<if test="'+data_5_2[i][1]+' != null and '+data_5_2[i][1]+' != \'\' ">\n	'+data_5_2[i][0]+'')
+		if data_6_1:
+			for i in range(len(data_6_1)):
+				line=line.replace(data_6_1[i],'<if test="'+data_6_2[i][0]+' != null and '+data_6_2[i][0]+' != \'\' ">\n	'+data_6_2[i][1]+'')
+		if data_7_1:
+			for i in range(len(data_7_1)):
+				line=line.replace(data_7_1[i],'<if test="'+data_7_2[i][0]+' != null and '+data_7_2[i][0]+' != \'\' "')
+		if data_8:
+			for i in range(len(data_8)):
+				line=line.replace(data_8[i],'and')
+		if data_9:
+			for i in range(len(data_9)):
+				line=line.replace(data_9[i],'</if>')
+		if data_10:
+			for i in range(len(data_10)):
+				line=line.replace(data_1i[i],'where 1=1')
+		if data_11:
+			for i in range(len(data_11)):
+				line=line.replace(data_11[i],'')
+		if data_12:
+			for i in range(len(data_12)):
+				line=line.replace(data_12[i],'')
+		if data_13_1:
+			for i in range(len(data_13_1)):
+				line=line.replace(data_13_1[i],'#{'+data_13_2[i]+'}')
+		if data_14_1:
+			for i in range(len(data_14_1)):
+				line=line.replace(data_14_1[i],'${'+data_14_2[i]+'}')
+		f.write(line)
+	f.close()
+
+def preimport(path):
+				'''
+				打印一个目录下的所有文件夹和文件 
+				'''
+				Xfile=[]
+				for root, dirs, files in os.walk(path):
+					for file in files:
+						if os.path.splitext(file)[1] == '.xml':
+							#Xfile.append(os.path.join(root, file))
+							replacefile(os.path.join(root, file),file)
+				#for refile in Xfile:
+					#replacefile(refile)
+if   __name__ == "__main__":
+    print sys.argv[1]
+    preimport(sys.argv[1])
+	
